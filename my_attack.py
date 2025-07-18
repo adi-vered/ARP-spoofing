92dfc64700a7513b639aa500183ba8d5c2eed0b1
Adi Vered <veredfamily36@gmail.com>
2025-07-19 00:18:19 +0300

    initial

---
+++ my_attack.py
@@ -0,0 +1,9 @@
+import scapy.all as scapy
+
+target_ip = "" 
+self_ip = ""
+interface = ""
+
+def find_mac_address(ip):
+    packet = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")/scapy.arp(pdst = ip)
+    answer = scapy.srp(packet, iface=interface, timeout=2, verbose=False)[0]
