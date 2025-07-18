218bf0aea976f9bb8a97f6a5d7e3ee636e6fa7cd
Adi Vered <veredfamily36@gmail.com>
2025-07-19 00:23:32 +0300

    find mac address done

--- my_attack.py
+++ my_attack.py
@@ -7,3 +7,6 @@ interface = ""
 def find_mac_address(ip):
     packet = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")/scapy.arp(pdst = ip)
     answer = scapy.srp(packet, iface=interface, timeout=2, verbose=False)[0]
+    mac_address = answer[1].hwsrc
+    return mac_address
+ 
