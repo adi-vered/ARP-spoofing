05cc2ab10e36ea8491cc5dc3bbe9173f84666482
Adi Vered <veredfamily36@gmail.com>
2025-07-21 21:08:46 +0300

    deleting get_url and get_credentials

--- sniffer.py
+++ sniffer.py
@@ -1,11 +1,10 @@
 import scapy.all as scapy
 from scapy.layers import http
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_packet)
+def sniff_packets(interface):
+    scapy.sniff(store=False, prn=process_packet)
 
 def process_packet(packet):
-    print("new packet")
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print(f"url is {url}")
@@ -13,15 +12,5 @@ def process_packet(packet):
         if cred:
             print("Print possible credential info")
 
-def get_url(packet):
-    return (packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path).decode("utf-8")
-
-keywords = {"username", "user", "password", "login", "uname", "signin", "signup", "name", "pass"}
-def get_credentials(packet):
-    if packet.haslayer(scapy.Raw):
-        field_load = packet[scapy.Raw].load.decode("utf-8")
-        for keyword in keywords:
-            if keyword in field_load:
-                return field_load
-
-sniff("Wi-Fi")
+sniff_packets("Ether")
+# print(scapy.interfaces.get_if_list())
